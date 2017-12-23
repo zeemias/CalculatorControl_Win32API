27143d578d38d296e76870ed94f268da04f416a9
Fauzat Galimardanov <mpr.pass@yandex.ru>
2017-12-23 23:19:56 +0300

    Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,428 @@
+﻿namespace WinApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.prc = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.delx = new System.Windows.Forms.Button();
+            this.ce = new System.Windows.Forms.Button();
+            this.devat = new System.Windows.Forms.Button();
+            this.vosem = new System.Windows.Forms.Button();
+            this.c = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.shest = new System.Windows.Forms.Button();
+            this.pyat = new System.Windows.Forms.Button();
+            this.ymnzh = new System.Windows.Forms.Button();
+            this.chetiri = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.sem = new System.Windows.Forms.Button();
+            this.delenie = new System.Windows.Forms.Button();
+            this.korn = new System.Windows.Forms.Button();
+            this.zap = new System.Windows.Forms.Button();
+            this.rovn = new System.Windows.Forms.Button();
+            this.nol = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.plusminus = new System.Windows.Forms.Button();
+            this.tri = new System.Windows.Forms.Button();
+            this.dva = new System.Windows.Forms.Button();
+            this.odin = new System.Windows.Forms.Button();
+            this.mc = new System.Windows.Forms.Button();
+            this.mr = new System.Windows.Forms.Button();
+            this.ms = new System.Windows.Forms.Button();
+            this.mp = new System.Windows.Forms.Button();
+            this.mm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prc
+            // 
+            this.prc.Location = new System.Drawing.Point(196, 110);
+            this.prc.Name = "prc";
+            this.prc.Size = new System.Drawing.Size(40, 30);
+            this.prc.TabIndex = 0;
+            this.prc.Text = "%";
+            this.prc.UseVisualStyleBackColor = true;
+            this.prc.Click += new System.EventHandler(this.prc_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // delx
+            // 
+            this.delx.Location = new System.Drawing.Point(196, 146);
+            this.delx.Name = "delx";
+            this.delx.Size = new System.Drawing.Size(40, 30);
+            this.delx.TabIndex = 2;
+            this.delx.Text = "1/x";
+            this.delx.UseVisualStyleBackColor = true;
+            this.delx.Click += new System.EventHandler(this.delx_Click);
+            // 
+            // ce
+            // 
+            this.ce.Location = new System.Drawing.Point(58, 74);
+            this.ce.Name = "ce";
+            this.ce.Size = new System.Drawing.Size(40, 30);
+            this.ce.TabIndex = 3;
+            this.ce.Text = "CE";
+            this.ce.UseVisualStyleBackColor = true;
+            this.ce.Click += new System.EventHandler(this.ce_Click);
+            // 
+            // devat
+            // 
+            this.devat.Location = new System.Drawing.Point(104, 110);
+            this.devat.Name = "devat";
+            this.devat.Size = new System.Drawing.Size(40, 30);
+            this.devat.TabIndex = 4;
+            this.devat.Text = "9";
+            this.devat.UseVisualStyleBackColor = true;
+            this.devat.Click += new System.EventHandler(this.devat_Click);
+            // 
+            // vosem
+            // 
+            this.vosem.Location = new System.Drawing.Point(58, 110);
+            this.vosem.Name = "vosem";
+            this.vosem.Size = new System.Drawing.Size(40, 30);
+            this.vosem.TabIndex = 5;
+            this.vosem.Text = "8";
+            this.vosem.UseVisualStyleBackColor = true;
+            this.vosem.Click += new System.EventHandler(this.vosem_Click);
+            // 
+            // c
+            // 
+            this.c.Location = new System.Drawing.Point(104, 74);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(40, 30);
+            this.c.TabIndex = 6;
+            this.c.Text = "C";
+            this.c.UseVisualStyleBackColor = true;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(150, 182);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(40, 30);
+            this.minus.TabIndex = 7;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // shest
+            // 
+            this.shest.Location = new System.Drawing.Point(104, 146);
+            this.shest.Name = "shest";
+            this.shest.Size = new System.Drawing.Size(40, 30);
+            this.shest.TabIndex = 8;
+            this.shest.Text = "6";
+            this.shest.UseVisualStyleBackColor = true;
+            this.shest.Click += new System.EventHandler(this.shest_Click);
+            // 
+            // pyat
+            // 
+            this.pyat.Location = new System.Drawing.Point(58, 146);
+            this.pyat.Name = "pyat";
+            this.pyat.Size = new System.Drawing.Size(40, 30);
+            this.pyat.TabIndex = 9;
+            this.pyat.Text = "5";
+            this.pyat.UseVisualStyleBackColor = true;
+            this.pyat.Click += new System.EventHandler(this.pyat_Click);
+            // 
+            // ymnzh
+            // 
+            this.ymnzh.Location = new System.Drawing.Point(150, 146);
+            this.ymnzh.Name = "ymnzh";
+            this.ymnzh.Size = new System.Drawing.Size(40, 30);
+            this.ymnzh.TabIndex = 10;
+            this.ymnzh.Text = "*";
+            this.ymnzh.UseVisualStyleBackColor = true;
+            this.ymnzh.Click += new System.EventHandler(this.ymnzh_Click);
+            // 
+            // chetiri
+            // 
+            this.chetiri.Location = new System.Drawing.Point(12, 146);
+            this.chetiri.Name = "chetiri";
+            this.chetiri.Size = new System.Drawing.Size(40, 30);
+            this.chetiri.TabIndex = 11;
+            this.chetiri.Text = "4";
+            this.chetiri.UseVisualStyleBackColor = true;
+            this.chetiri.Click += new System.EventHandler(this.chetiri_Click);
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(12, 74);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(40, 30);
+            this.del.TabIndex = 12;
+            this.del.Text = "<---";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // sem
+            // 
+            this.sem.Location = new System.Drawing.Point(12, 110);
+            this.sem.Name = "sem";
+            this.sem.Size = new System.Drawing.Size(40, 30);
+            this.sem.TabIndex = 13;
+            this.sem.Text = "7";
+            this.sem.UseVisualStyleBackColor = true;
+            this.sem.Click += new System.EventHandler(this.sem_Click);
+            // 
+            // delenie
+            // 
+            this.delenie.Location = new System.Drawing.Point(150, 110);
+            this.delenie.Name = "delenie";
+            this.delenie.Size = new System.Drawing.Size(40, 30);
+            this.delenie.TabIndex = 14;
+            this.delenie.Text = "/";
+            this.delenie.UseVisualStyleBackColor = true;
+            this.delenie.Click += new System.EventHandler(this.delenie_Click);
+            // 
+            // korn
+            // 
+            this.korn.Location = new System.Drawing.Point(196, 74);
+            this.korn.Name = "korn";
+            this.korn.Size = new System.Drawing.Size(40, 30);
+            this.korn.TabIndex = 16;
+            this.korn.Text = "√";
+            this.korn.UseVisualStyleBackColor = true;
+            this.korn.Click += new System.EventHandler(this.korn_Click);
+            // 
+            // zap
+            // 
+            this.zap.Location = new System.Drawing.Point(104, 218);
+            this.zap.Name = "zap";
+            this.zap.Size = new System.Drawing.Size(40, 30);
+            this.zap.TabIndex = 17;
+            this.zap.Text = ",";
+            this.zap.UseVisualStyleBackColor = true;
+            this.zap.Click += new System.EventHandler(this.zap_Click);
+            // 
+            // rovn
+            // 
+            this.rovn.Location = new System.Drawing.Point(196, 182);
+            this.rovn.Name = "rovn";
+            this.rovn.Size = new System.Drawing.Size(40, 66);
+            this.rovn.TabIndex = 18;
+            this.rovn.Text = "=";
+            this.rovn.UseVisualStyleBackColor = true;
+            this.rovn.Click += new System.EventHandler(this.rovn_Click);
+            // 
+            // nol
+            // 
+            this.nol.Location = new System.Drawing.Point(12, 218);
+            this.nol.Name = "nol";
+            this.nol.Size = new System.Drawing.Size(86, 30);
+            this.nol.TabIndex = 19;
+            this.nol.Text = "0";
+            this.nol.UseVisualStyleBackColor = true;
+            this.nol.Click += new System.EventHandler(this.nol_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(150, 218);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(40, 30);
+            this.plus.TabIndex = 20;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // plusminus
+            // 
+            this.plusminus.Location = new System.Drawing.Point(150, 74);
+            this.plusminus.Name = "plusminus";
+            this.plusminus.Size = new System.Drawing.Size(40, 30);
+            this.plusminus.TabIndex = 21;
+            this.plusminus.Text = "±";
+            this.plusminus.UseVisualStyleBackColor = true;
+            this.plusminus.Click += new System.EventHandler(this.plusminus_Click);
+            // 
+            // tri
+            // 
+            this.tri.Location = new System.Drawing.Point(104, 182);
+            this.tri.Name = "tri";
+            this.tri.Size = new System.Drawing.Size(40, 30);
+            this.tri.TabIndex = 22;
+            this.tri.Text = "3";
+            this.tri.UseVisualStyleBackColor = true;
+            this.tri.Click += new System.EventHandler(this.tri_Click);
+            // 
+            // dva
+            // 
+            this.dva.Location = new System.Drawing.Point(58, 182);
+            this.dva.Name = "dva";
+            this.dva.Size = new System.Drawing.Size(40, 30);
+            this.dva.TabIndex = 23;
+            this.dva.Text = "2";
+            this.dva.UseVisualStyleBackColor = true;
+            this.dva.Click += new System.EventHandler(this.dva_Click);
+            // 
+            // odin
+            // 
+            this.odin.Location = new System.Drawing.Point(12, 182);
+            this.odin.Name = "odin";
+            this.odin.Size = new System.Drawing.Size(40, 30);
+            this.odin.TabIndex = 24;
+            this.odin.Text = "1";
+            this.odin.UseVisualStyleBackColor = true;
+            this.odin.Click += new System.EventHandler(this.odin_Click);
+            // 
+            // mc
+            // 
+            this.mc.Location = new System.Drawing.Point(12, 38);
+            this.mc.Name = "mc";
+            this.mc.Size = new System.Drawing.Size(40, 30);
+            this.mc.TabIndex = 25;
+            this.mc.Text = "MC";
+            this.mc.UseVisualStyleBackColor = true;
+            this.mc.Click += new System.EventHandler(this.mc_Click);
+            // 
+            // mr
+            // 
+            this.mr.Location = new System.Drawing.Point(58, 38);
+            this.mr.Name = "mr";
+            this.mr.Size = new System.Drawing.Size(40, 30);
+            this.mr.TabIndex = 26;
+            this.mr.Text = "MR";
+            this.mr.UseVisualStyleBackColor = true;
+            this.mr.Click += new System.EventHandler(this.mr_Click);
+            // 
+            // ms
+            // 
+            this.ms.Location = new System.Drawing.Point(104, 38);
+            this.ms.Name = "ms";
+            this.ms.Size = new System.Drawing.Size(40, 30);
+            this.ms.TabIndex = 27;
+            this.ms.Text = "MS";
+            this.ms.UseVisualStyleBackColor = true;
+            this.ms.Click += new System.EventHandler(this.ms_Click);
+            // 
+            // mp
+            // 
+            this.mp.Location = new System.Drawing.Point(150, 38);
+            this.mp.Name = "mp";
+            this.mp.Size = new System.Drawing.Size(40, 30);
+            this.mp.TabIndex = 28;
+            this.mp.Text = "M+";
+            this.mp.UseVisualStyleBackColor = true;
+            this.mp.Click += new System.EventHandler(this.mp_Click);
+            // 
+            // mm
+            // 
+            this.mm.Location = new System.Drawing.Point(196, 38);
+            this.mm.Name = "mm";
+            this.mm.Size = new System.Drawing.Size(40, 30);
+            this.mm.TabIndex = 29;
+            this.mm.Text = "M-";
+            this.mm.UseVisualStyleBackColor = true;
+            this.mm.Click += new System.EventHandler(this.mm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 259);
+            this.Controls.Add(this.mm);
+            this.Controls.Add(this.mp);
+            this.Controls.Add(this.ms);
+            this.Controls.Add(this.mr);
+            this.Controls.Add(this.mc);
+            this.Controls.Add(this.odin);
+            this.Controls.Add(this.dva);
+            this.Controls.Add(this.tri);
+            this.Controls.Add(this.plusminus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.nol);
+            this.Controls.Add(this.rovn);
+            this.Controls.Add(this.zap);
+            this.Controls.Add(this.korn);
+            this.Controls.Add(this.delenie);
+            this.Controls.Add(this.sem);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.chetiri);
+            this.Controls.Add(this.ymnzh);
+            this.Controls.Add(this.pyat);
+            this.Controls.Add(this.shest);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.vosem);
+            this.Controls.Add(this.devat);
+            this.Controls.Add(this.ce);
+            this.Controls.Add(this.delx);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.prc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CalculatorControl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button prc;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button delx;
+        private System.Windows.Forms.Button ce;
+        private System.Windows.Forms.Button devat;
+        private System.Windows.Forms.Button vosem;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button shest;
+        private System.Windows.Forms.Button pyat;
+        private System.Windows.Forms.Button ymnzh;
+        private System.Windows.Forms.Button chetiri;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button sem;
+        private System.Windows.Forms.Button delenie;
+        private System.Windows.Forms.Button korn;
+        private System.Windows.Forms.Button zap;
+        private System.Windows.Forms.Button rovn;
+        private System.Windows.Forms.Button nol;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button plusminus;
+        private System.Windows.Forms.Button tri;
+        private System.Windows.Forms.Button dva;
+        private System.Windows.Forms.Button odin;
+        private System.Windows.Forms.Button mc;
+        private System.Windows.Forms.Button mr;
+        private System.Windows.Forms.Button ms;
+        private System.Windows.Forms.Button mp;
+        private System.Windows.Forms.Button mm;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinApi
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindow(IntPtr hWnd, int uCmd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, [Out] StringBuilder lParam);
+
+		//Перемещение курсора
+        const int WM_MOUSEMOVE = 0x0200;
+        //Нажатие левой кнопки мыши
+        const int WM_LBUTTONDOWN = 0x0201;
+        //Отпускание левой кнопки мыши
+        const int WM_LBUTTONUP = 0x0202;
+        //Найденный дескриптор идентифицирует окно того же самого типа, которое является самым высоким в Z - последовательности.
+        const int GW_HWNDFIRST = 0;
+        //Найденный дескриптор идентифицирует окно ниже определяемого окна в Z - последовательности.
+        const int GW_HWNDNEXT = 2;
+        //Извлеченный дескриптор идентифицирует дочернее окно наверху Z - последовательности.
+        const int GW_CHILD = 5;
+        //Получение текста из окна
+        const int WM_GETTEXT = 0x000D;
+
+        //Массив дескрипторов кнопок
+        public IntPtr[] buttons = new IntPtr[28];
+        //Дескриптор окна с ответом
+        IntPtr dialogFrame = new IntPtr();
+
+        public Form1()
+        {
+            InitializeComponent();
+			//Находим дескриптор главного окна калькулятора по нозванию окна          
+            IntPtr calcWnd = FindWindow(null, "Калькулятор");
+            //Пока окно не найдено, выводим сообщение об ошибке, когда найдет, продолжит работу 
+            while (calcWnd.ToInt32() == 0)
+            {
+            	//Сообщение об ошибке
+                MessageBox.Show("Калькулятор закрыт! Откройте калькулятор и нажмите Ок!");
+                //Поиск дескриптора
+                calcWnd = FindWindow(null, "Калькулятор");
+            }
+            //Получаем дескриптор дочернего окна калькулятора
+            IntPtr calcChild = GetWindow(calcWnd, GW_CHILD);
+            //Получаем дескриптор следующего дочернего окна
+            IntPtr frameChild = GetWindow(calcChild, GW_CHILD);
+            //Получаем дескриптор следующего окна
+            IntPtr nextChild = GetWindow(frameChild, GW_HWNDNEXT);
+            //Получаем дескриптор следующего окна, в котором содержутся кнопки
+            IntPtr buttonsFrame = GetWindow(nextChild, GW_HWNDNEXT);
+            //Записываем кнопки калькулятора в массив дескрипторов
+            buttons[0] = GetWindow(buttonsFrame, GW_CHILD);
+            for(int i = 1; i < 28; i++)
+            {
+                buttons[i] = GetWindow(buttons[i-1], GW_HWNDNEXT);
+            }
+            IntPtr firstFrame = GetWindow(nextChild, GW_CHILD);
+            IntPtr secondFrame = GetWindow(firstFrame, GW_HWNDNEXT);
+            //Получаем дескриптор окна с ответом
+            dialogFrame = GetWindow(secondFrame, GW_HWNDNEXT);
+            //Получаем текст из окна с ответом
+            showDialog(dialogFrame);
+        }
+
+        //Функция для получения текста из окна с ответом (принимает дискриптор окна с ответом)
+        public void showDialog(IntPtr dialog)
+        {
+        	//Создаем строку для ответа
+            StringBuilder title = new StringBuilder();
+            //Получаем текст из окна с ответом
+            SendMessage(dialog, WM_GETTEXT, (IntPtr)20, title);
+            //Выводим текст в наше приложение
+            textBox1.Text = title.ToString();
+        }
+
+        //Функция для нажатия на кнопку (принимает дискриптор кнопки)
+        public void butClick(IntPtr but)
+        {
+        	//Наводим курсор на кнопку
+            SendMessage(but, WM_MOUSEMOVE, 0, 0);
+            //Нажимаем на левую кнопку мыши
+            SendMessage(but, WM_LBUTTONDOWN, 0, 0);
+            //Отпускаем левую кнопку мыши
+            SendMessage(but, WM_LBUTTONUP, 0, 0);
+        }
+
+        private void prc_Click(object sender, EventArgs e)
+        {
+        	//Нажимаем на кнопку
+            butClick(buttons[25]);
+            //Получаем ответ
+            showDialog(dialogFrame);
+        }
+
+        private void korn_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[24]);
+            showDialog(dialogFrame);
+        }
+
+        private void delx_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[26]);
+            showDialog(dialogFrame);
+        }
+
+        private void ce_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[7]);
+            showDialog(dialogFrame);
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[12]);
+            showDialog(dialogFrame);
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[1]);
+            showDialog(dialogFrame);
+        }
+
+        private void delenie_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[19]);
+            showDialog(dialogFrame);
+        }
+
+        private void sem_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[2]);
+            showDialog(dialogFrame);
+        }
+
+        private void vosem_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[8]);
+            showDialog(dialogFrame);
+        }
+
+        private void devat_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[13]);
+            showDialog(dialogFrame);
+        }
+
+        private void ymnzh_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[20]);
+            showDialog(dialogFrame);
+        }
+
+        private void chetiri_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[3]);
+            showDialog(dialogFrame);
+        }
+
+        private void pyat_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[9]);
+            showDialog(dialogFrame);
+        }
+
+        private void shest_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[14]);
+            showDialog(dialogFrame);
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[21]);
+            showDialog(dialogFrame);
+        }
+
+        private void odin_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[4]);
+            showDialog(dialogFrame);
+        }
+
+        private void dva_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[10]);
+            showDialog(dialogFrame);
+        }
+
+        private void tri_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[15]);
+            showDialog(dialogFrame);
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[22]);
+            showDialog(dialogFrame);
+        }
+
+        private void plusminus_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[18]);
+            showDialog(dialogFrame);
+        }
+
+        private void nol_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[5]);
+            showDialog(dialogFrame);
+        }
+
+        private void zap_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[16]);
+            showDialog(dialogFrame);
+        }
+
+        private void rovn_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[27]);
+            showDialog(dialogFrame);
+        }
+
+        private void mc_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[0]);
+            showDialog(dialogFrame);
+        }
+
+        private void mr_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[6]);
+            showDialog(dialogFrame);
+        }
+
+        private void ms_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[11]);
+            showDialog(dialogFrame);
+        }
+
+        private void mp_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[17]);
+            showDialog(dialogFrame);
+        }
+
+        private void mm_Click(object sender, EventArgs e)
+        {
+            butClick(buttons[23]);
+            showDialog(dialogFrame);
+        }
+    }
+}
